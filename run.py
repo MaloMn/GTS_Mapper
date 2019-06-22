bc2d28d88eca031019f8d6ace917f0fce14421c9
MaloMn <malo.maisonneuve@gmail.com>
2019-06-22 21:43:31 +0200

    treatment done

--- run.py
+++ run.py
@@ -1,46 +1,61 @@
+import re
+
 codes = ['c', '"', 'l', '}', 'v', ']]']
+output = ''
 
 # Getting the data
 with open('origin.txt', 'r') as f:
     content = f.read()
 
-GTS = []
+b = 0
+while content.find('"c"') >= 0:
+    GTS = []
+    
+    for i in range(0, len(codes), 2):
+        a = content.find('"' + codes[i] + '"')
+        content = content[a+5:]
+    
+        b = content.find(codes[i+1])
+        GTS.append(content[:b])
+    
+        content = content[b:]
+    
+    # Collecting the labels
+    reg = re.compile('"([^"]*)"')
+    st = GTS[1]
+    m = reg.findall(st)
+    
+    
+    GTS[1] = m
+    
+    # Collecting the values
+    reg = re.compile(r'\d+')
+    st = GTS[2]
+    m = reg.findall(st)
+    
+    GTS[2] = m
+    
+    # WRITING THE DATA ============
+    st_out = '[ NEWGTS '
+    
+    # Add the class
+    st_out += '"' + GTS[0] + str(b) + '"'
+    st_out += ' RENAME { '
+    
+    # Add the labels
+    for word in GTS[1]:
+        st_out += '"' + word + '"'
+        st_out += ' '
+    
+    st_out += '} RELABEL '
+    
+    # Add the values
+    for i in range(0, len(GTS[2]), 2):
+        st_out += GTS[2][i] + ' NaN NaN NaN ' + GTS[2][i+1] + ' ADDVALUE '
+    
+    output += st_out
+    
+    b += 1
+    
+output += ']'
 
-for i in range(0,len(codes), 2):
-    a = content.find('"' + codes[i] + '"')
-    content = content[a+5:]
-
-    b = content.find(codes[i+1])
-    GTS.append(content[:b])
-
-    content = content[b:]
-
-import re
-
-# Collecting the labels
-labels = []
-reg = re.compile('"([^"]*)"')
-st = GTS[1]
-m = reg.match(st)
-
-while m:
-    labels.append(m.group()[1:-1])
-    st=st[m.end()+1:]
-    m = reg.match(st)
-
-GTS[1] = labels
-
-# Collecting the values
-values = []
-reg = re.compile('([0-9])\w+')
-st = GTS[2]
-m = reg.match(st)
-
-while m:
-    values.append(m.group())
-    st=st[m.end():]
-    m = reg.match(st)
-
-GTS[2] = values
-
-print(GTS)
